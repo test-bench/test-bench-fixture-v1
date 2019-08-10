467c07f355c375b5e3a93b0803a665ae497bcde4
Nathan Ladd <nathanladd+github@gmail.com>
2019-08-09 22:03:30 -0500

    Initial commit

---
+++ test/test_init.rb
@@ -0,0 +1,13 @@
+require_relative '../init'
+
+unless ENV['BOOTSTRAP'] == 'off'
+  require 'test_bench/bootstrap'; TestBench::Bootstrap.activate
+else
+  require 'test_bench'; TestBench.activate
+end
+
+require 'test_bench/fixture/controls'
+
+include TestBench
+
+Controls = Fixture::Controls if defined?(Fixture::Controls)
