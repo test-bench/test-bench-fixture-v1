48851de4f175412602c7cd198917abcf6c646c20
Nathan Ladd <nathanladd+github@gmail.com>
2019-08-11 18:17:03 -0500

    Run: Failed Predicate

---
+++ lib/test_bench/fixture/run.rb
@@ -0,0 +1,34 @@
+module TestBench
+  module Fixture
+    class Run
+      def assertion_counter
+        @assertion_counter ||= 0
+      end
+      attr_writer :assertion_counter
+
+      def error_counter
+        @error_counter ||= 0
+      end
+      attr_writer :error_counter
+
+      def depth
+        @depth ||= 0
+      end
+      attr_writer :depth
+
+      def error_policy
+        @error_policy ||= ErrorPolicy::Build.(:rescue_assert)
+      end
+      attr_writer :error_policy
+
+      def output
+        @output ||= Output::Substitute.build
+      end
+      attr_writer :output
+
+      def failed?
+        error_counter.nonzero?
+      end
+    end
+  end
+end
