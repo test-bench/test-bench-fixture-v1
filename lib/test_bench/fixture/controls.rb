6a2b07c9b21189b1d9c1d8edfb6aaedd41b0f11e
Nathan Ladd <nathanladd+github@gmail.com>
2019-08-11 15:28:53 -0500

    Controls: Result

--- lib/test_bench/fixture/controls.rb
+++ lib/test_bench/fixture/controls.rb
@@ -1,3 +1,4 @@
 require 'test_bench/fixture/controls/caller_location'
 require 'test_bench/fixture/controls/error/backtrace'
 require 'test_bench/fixture/controls/error'
+require 'test_bench/fixture/controls/result'
