b6c205e3e60ce17a27dd5a4a1fdd10013253b4ea
Nathan Ladd <nathanladd+github@gmail.com>
2019-08-11 15:39:50 -0500

    Controls: Error

--- lib/test_bench/fixture/controls.rb
+++ lib/test_bench/fixture/controls.rb
@@ -3,3 +3,5 @@ unless RUBY_ENGINE == 'mruby'
 end
 
 require 'test_bench/fixture/controls/caller_location'
+require 'test_bench/fixture/controls/error/backtrace'
+require 'test_bench/fixture/controls/error'
