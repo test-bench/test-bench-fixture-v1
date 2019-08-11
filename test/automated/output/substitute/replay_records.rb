5094c353049b77d6011e78b645d6ca27194dd022
Nathan Ladd <nathanladd+github@gmail.com>
2019-08-11 17:34:51 -0500

    Output (Substitute): Replay Records

---
+++ test/automated/output/substitute/replay_records.rb
@@ -0,0 +1,37 @@
+require_relative '../../automated_init'
+
+context "Output" do
+  context "Substitute" do
+    context "Replay Records" do
+      substitute = Fixture::Output::Substitute.build
+
+      result = Controls::Result.example
+      caller_location = Controls::CallerLocation.example
+      error = Controls::Error.example
+
+      {
+        :assert => [result, caller_location],
+        :enter_assert_block => [caller_location],
+        :exit_assert_block => [caller_location, result],
+        :comment => ["Some text"],
+        :error => [error],
+        :start_test => ["Some test"],
+        :finish_test => ["Some test", result],
+        :skip_test => ["Some test"],
+        :enter_context => ["Some Context"],
+        :exit_context => ["Some Context", result],
+        :skip_context => ["Some Context"]
+      }.each do |method_name, arguments|
+        substitute.public_send(method_name, *arguments)
+      end
+
+      output = Fixture::Output::Substitute.build
+
+      substitute.replay_records(output)
+
+      test "All records are dispatched to given output" do
+        assert(output.records == substitute.records)
+      end
+    end
+  end
+end
