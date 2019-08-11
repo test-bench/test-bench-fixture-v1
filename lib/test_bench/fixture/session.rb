f65fe19bee43c6915cf909459046dc6c899ca947
Nathan Ladd <nathanladd+github@gmail.com>
2019-08-11 18:17:03 -0500

    Session: Failed Predicate

---
+++ lib/test_bench/fixture/session.rb
@@ -0,0 +1,18 @@
+module TestBench
+  module Fixture
+    class Session
+      def error_counter
+        @error_counter ||= 0
+      end
+      attr_writer :error_counter
+
+      def fail!
+        self.error_counter += 1
+      end
+
+      def failed?
+        error_counter.nonzero? ? true : false
+      end
+    end
+  end
+end
