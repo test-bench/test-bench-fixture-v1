54a689516baee514e114ec2030ac09d2b744cb92
Nathan Ladd <nathanladd+github@gmail.com>
2019-08-11 22:46:01 -0500

    Fixture: Actuator

---
+++ test/automated/fixture/actuator/initialize_method_accepts_block.rb
@@ -0,0 +1,38 @@
+require_relative '../../automated_init'
+
+context "Fixture" do
+  context "Actuator" do
+    context "Initialize Method Accepts Block" do
+      cls = Controls::Fixture.example_class do
+        attr_reader :blk
+
+        def initialize(&blk)
+          @blk = blk
+        end
+
+        def call
+        end
+      end
+
+      block_executed = false
+
+      block_executions = 0
+
+      fixture = Fixture.(cls) do
+        block_executed = true
+        block_executions += 1
+      end
+
+      test "Block is passed to fixture's initializer" do
+        fixture.blk.()
+
+        assert(block_executed == true)
+      end
+
+      test "Block is not executed again when fixture is actuated" do
+        assert(block_executions == 1)
+      end
+    end
+  end
+end
+
