b1d6ce55046683a4f24b754e1e1047dbbc964def
Nathan Ladd <nathanladd+github@gmail.com>
2019-08-11 22:23:52 -0500

    Fixture: Build

---
+++ lib/test_bench/fixture/fixture.rb
@@ -0,0 +1,24 @@
+module TestBench
+  module Fixture
+    def self.build(cls, *args, run: nil, output: nil, error_policy: nil, factory_method: nil, **kwargs)
+      if cls.respond_to?(:build)
+        factory_method ||= :build
+      else
+        factory_method ||= :new
+      end
+
+      args << kwargs unless kwargs.empty?
+
+      instance = cls.public_send(factory_method, *args)
+      Run.configure(instance, run: run, output: output, error_policy: error_policy)
+      instance
+    end
+
+    def test_run
+      @test_run ||= Run::Substitute.build
+    end
+    attr_writer :test_run
+    alias_method :run, :test_run
+    alias_method :run=, :test_run=
+  end
+end
