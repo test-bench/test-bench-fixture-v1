e75f4e9c34e4b94fabd57d8c58f374d3681db328
Nathan Ladd <nathanladd+github@gmail.com>
2019-08-13 20:04:26 -0500

    Fixture (Instance Methods): Refute

--- lib/test_bench/fixture/fixture.rb
+++ lib/test_bench/fixture/fixture.rb
@@ -71,5 +71,11 @@ module TestBench
 
       test_session.assert(value, caller_location: caller_location)
     end
+
+    def refute(value, caller_location: nil)
+      caller_location ||= caller_locations.first
+
+      test_session.assert(!value, caller_location: caller_location)
+    end
   end
 end
