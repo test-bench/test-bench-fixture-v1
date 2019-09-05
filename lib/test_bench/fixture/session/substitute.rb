7ed7c748f21e8174148b6b39f8d072d748645dd4
Nathan Ladd <nathanladd+github@gmail.com>
2019-09-04 21:15:17 -0500

    Session Substitute (Test Predicates): One Passed Predicate

--- lib/test_bench/fixture/session/substitute.rb
+++ lib/test_bench/fixture/session/substitute.rb
@@ -82,6 +82,10 @@ module TestBench
             passes.first
           end
 
+          def one_passed?(*titles)
+            one_pass(*titles) ? true : false
+          end
+
           def pass(*titles)
             passes(*titles).first
           end
