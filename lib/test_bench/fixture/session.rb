00a19eba1a39fead9736e8db5a9bdbf55d49520b
Nathan Ladd <nathanladd@gmail.com>
2020-01-10 20:21:00 -0600

    Session: Start

--- lib/test_bench/fixture/session.rb
+++ lib/test_bench/fixture/session.rb
@@ -1,6 +1,8 @@
 module TestBench
   module Fixture
     class Session
+      Error = Class.new(RuntimeError)
+
       def assertion_counter
         @assertion_counter ||= 0
       end
@@ -10,6 +12,29 @@ module TestBench
         @error_counter ||= 0
       end
       attr_writer :error_counter
+
+      def started
+        instance_variable_defined?(:@started) ?
+          @started :
+          @started = false
+      end
+      attr_writer :started
+      alias_method :started?, :started
+
+      def output
+        @output ||= Output::Substitute.build
+      end
+      attr_writer :output
+
+      def start
+        if started
+          raise Error, "Session has already been started"
+        end
+
+        self.started = true
+
+        output.start
+      end
 
       def fail!
         self.assertion_counter += 1
