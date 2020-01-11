b8320ffe034a344e94ba9c177e0d0088e8194df6
Nathan Ladd <nathanladd@gmail.com>
2020-01-10 21:02:03 -0600

    Session: Load

--- lib/test_bench/fixture/session.rb
+++ lib/test_bench/fixture/session.rb
@@ -104,6 +104,20 @@ module TestBench
         result
       end
 
+      def load(path)
+        output.enter_file(path)
+
+        result = false
+
+        Kernel.load(path)
+
+        result = true
+
+      ensure
+
+        output.exit_file(path, result)
+      end
+
       def evaluate(action, &block)
         previous_error_counter = self.error_counter
 
