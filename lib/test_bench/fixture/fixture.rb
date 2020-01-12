47a78ff82bf2c8f5ecf8f814ffe7eddc69545987
Nathan Ladd <nathanladd@gmail.com>
2020-01-11 21:33:35 -0600

    Fixture: Build

--- lib/test_bench/fixture/fixture.rb
+++ lib/test_bench/fixture/fixture.rb
@@ -2,6 +2,20 @@ module TestBench
   module Fixture
     Error = Class.new(RuntimeError)
 
+    def self.build(cls, *args, session: nil, output: nil, error_policy: nil, factory_method: nil, **kwargs, &block)
+      factory_method = factory_method(cls)
+
+      if kwargs.empty?
+        instance = factory_method.(*args, &block)
+      else
+        instance = factory_method.(*args, **kwargs, &block)
+      end
+
+      Session.configure(instance, session: session, output: output, error_policy: error_policy)
+
+      instance
+    end
+
     def self.factory_method(cls)
       if cls.respond_to?(:build)
         cls.method(:build)
@@ -11,5 +25,12 @@ module TestBench
         raise Error, "Must be given a class (Argument: #{cls.inspect})"
       end
     end
+
+    def test_session
+      @test_session ||= Session::Substitute.build
+    end
+    attr_writer :test_session
+    alias_method :session, :test_session
+    alias_method :session=, :test_session=
   end
 end
