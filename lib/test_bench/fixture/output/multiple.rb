d049b75d3b1e287a8f191d4d439ef735dbbafbc8
Nathan Ladd <nathanladd@gmail.com>
2020-02-21 09:40:06 -0600

    Output (Multiple): Register Output

---
+++ lib/test_bench/fixture/output/multiple.rb
@@ -0,0 +1,22 @@
+module TestBench
+  module Fixture
+    module Output
+      class Multiple
+        include Output
+
+        def outputs
+          @outputs ||= []
+        end
+        attr_writer :outputs
+
+        def register(output)
+          outputs << output
+        end
+
+        def registered?(output)
+          outputs.include?(output)
+        end
+      end
+    end
+  end
+end
